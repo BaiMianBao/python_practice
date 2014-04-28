78033e106920808aa0b67cf5bad785b626b7c33c
BaiMianBao <steve.rj.bell@gmail.com>
2014-04-27 19:21:24 -0700

    Syncing all of the google practice stuff
    Google practice exercises

--- 16.22_word_search.py
+++ 16.22_word_search.py
@@ -11,6 +11,10 @@
     S M A R
 """
 
+def is_prefix_valid(search_word):
+  return search_word.lookup(search_word)
+  
+
 english_dictionary = {}
 
 search_word = []
@@ -21,7 +25,22 @@ boggle_board[0].append("L")
 boggle_board[0].append("I")
 boggle_board[0].append("G")
 boggle_board[0].append("O")
+boggle_board[1].append("E")
+boggle_board[1].append("P")
+boggle_board[1].append("N")
+boggle_board[1].append("I")
 
+while letter_x < length(boggle_board[0]):
+  while letter_y < 2:
+     # assumes the board is the same dimensions in x&y
+     print boggle_board[letter_x][letter_y]
+
+     search_word.append boggle_board[letter_x][letter_y]
+
+     # find out if there is a word with this prefix
+     if (is_prefix_valid(search_word)):
+       
+     
 
 print boggle_board
 
