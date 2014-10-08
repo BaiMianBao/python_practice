98c58fbe60e1d2f634872fb7cd2222d8b178d36e
BaiMianBao <steve.rj.bell@gmail.com>
2014-10-08 09:09:37 -0700

    add probabilities, not working yet

---
+++ markov_chain/markov.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python
+
+class Student:
+    def _init_(self, name):
+        self.name = name
+        self.attend = 0
+        self.grades = []
+        print("Hi!  My name is (0)".format(self.name))
+
