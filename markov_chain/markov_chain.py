e04df76afc7e65d69b77dbe24efad1f5ba983b8f
BaiMianBao <steve.rj.bell@gmail.com>
2014-10-27 23:25:18 -0700

    camelCase, cleaned up some OO errors, first round of Mike's code review

--- markov_chain/markov_chain.py
+++ markov_chain/markov_chain.py
@@ -27,23 +27,23 @@ You must implement the methods buildModel and printStats.  If you have time, do 
 class Model:
   def __init__(self):
     self.wordList = ""
-    # char_couplets is the dictionary where we'll be storing the 
+    # charCouplets is the dictionary where we'll be storing the 
     # pairs of characters and the number of times they appear.  
     # using this we'll be able to calculate the probability of one
     # char following another.
-    self.char_couplets = {}
+    self.charCouplets = {}
 
-    # char_starts is the dict where we keep track of all of the 
+    # charStarts is the dict where we keep track of all of the 
     # letters which are followed by another letter
-    self.char_starts = {}
+    self.charStarts = {}
 
-    # word_starts is the dict where we keep track of all of the 
+    # wordStarts is the dict where we keep track of all of the 
     # letters which start words
-    self.word_starts = {}
+    self.wordStarts = {}
 
-    # word_ends is the dict where we keep track of all of the 
+    # wordEnds is the dict where we keep track of all of the 
     # letters which end words
-    self.word_ends = {}
+    self.wordEnds = {}
 
   def printList(self, wordList):
     print "DEBUG:  wordList is " + str(wordList)
@@ -61,76 +61,75 @@ class Model:
     # denominator for our probabilities later.
     # It's nicer to do it in the same loop.
 
-    for word in model1.wordList:
+    for word in self.wordList:
       # print "DEBUG: word is " + word
       # i = counter for iterating through the word
       i = 0
       word_start = word[:1]
       word_end = word[-1:]
-      if word_start in model1.word_starts:
-        # print "DEBUG: start found" + str(model1.word_starts)
-        model1.word_starts[word_start] += 1.0
+      if word_start in self.wordStarts:
+        # print "DEBUG: start found" + str(self.wordStarts)
+        self.wordStarts[word_start] += 1.0
       else:
-        model1.word_starts[word_start] = 1.0
-        # print "DEBUG: start not found" + str(model1.word_starts)
+        self.wordStarts[word_start] = 1.0
+        # print "DEBUG: start not found" + str(self.wordStarts)
         
-      if word_end in model1.word_ends:
-        model1.word_ends[word_end] += 1.0
+      if word_end in self.wordEnds:
+        self.wordEnds[word_end] += 1.0
       else:
-        model1.word_ends[word_end] = 1.0
+        self.wordEnds[word_end] = 1.0
 
       while i < (len(word)-1):
         char_start = word[i:i+1]
         char_couple = word[i:i+2]
         # print "DEBUG: " + char_couple
   
-        if char_start in model1.char_starts:
-          model1.char_starts[char_start] += 1.0
+        if char_start in self.charStarts:
+          self.charStarts[char_start] += 1.0
         else:
-          model1.char_starts[char_start] = 1.0
-        if char_couple in model1.char_couplets:
-          model1.char_couplets[char_couple] += 1.0
+          self.charStarts[char_start] = 1.0
+        if char_couple in self.charCouplets:
+          self.charCouplets[char_couple] += 1.0
         else:
-          model1.char_couplets[char_couple] = 1.0
+          self.charCouplets[char_couple] = 1.0
         i += 1
     
     # MAINT:  this used to sort correctly, but now it doesn't.
     #         Apparently these get turned into lists after being
     #         sorted and lose their dictionaryness.  :(
-    # model1.char_couplets = sorted(model1.char_couplets)
-    # model1.char_starts = sorted(model1.char_starts)
-    # model1.word_starts = sorted(model1.word_starts)
-    return ()
+    # self.charCouplets = sorted(self.charCouplets)
+    # self.charStarts = sorted(self.charStarts)
+    # self.wordStarts = sorted(self.wordStarts)
 
-  def printStats(self, char_couplets, char_starts, word_starts):
+  def printStats(self):
     # Cool, now we can work out the probabilities by looking at 
     # the second letters.  
   
-    # print "DEBUG: " + str(model1.word_starts)
-    for start in model1.word_starts:
+    # print "DEBUG: " + str(self.wordStarts)
+    for start in self.wordStarts:
       # print "DEBUG: " + start
-      # print "DEBUG: " + str(model1.word_starts[start])
-      # print "DEBUG: " + str(len(model1.wordList))
-      word_start_probability = model1.word_starts[start]/len(model1.wordList) 
+      # print "DEBUG: " + str(self.wordStarts[start])
+      # print "DEBUG: " + str(len(self.wordList))
+      word_start_probability = self.wordStarts[start]/len(self.wordList) 
       # print "DEBUG: word_start_probability = " + str(word_start_probability)
       print str(start) + " starts a word " + \
       str("{0:.0f}%".format(word_start_probability*100)) + \
       " of the time"
 
-    for couplet in model1.char_couplets:
-      couplet_term1 = couplet[:1]                                             
-      couplet_term2 = couplet[1:2]
+    for couplet in self.charCouplets:
+      coupletTerm1 = couplet[:1]                                             
+      coupletTerm2 = couplet[1:2]
       # We can get the probabilities by dividing the number of 
       # couplets by the number of starts
       #
-      couplet_probability = char_couplets[couplet] / char_starts[couplet_term1]
-      print couplet_term1 + " is followed by " + couplet_term2 + \
-      " " + str("{0:.0f}%".format(couplet_probability*100)) + \
+      coupletProbability = self.charCouplets[couplet] / self.charStarts[coupletTerm1]
+      print coupletTerm1 + " is followed by " + coupletTerm2 + \
+      " " + str("{0:.0f}%".format(coupletProbability*100)) + \
       " of the time"
 
-    for end in model1.word_ends:
-      # print "DEBUG: model1.word_ends[end] " + str(model1.word_ends[end])
-      word_end_probability = model1.word_ends[end]/len(model1.wordList) 
+    for end in self.wordEnds:
+      # print "DEBUG: self.wordEnds[end] " + str(self.wordEnds[end])
+      word_end_probability = self.wordEnds[end]/len(self.wordList) 
       print str(end) + " ends a word " + \
       str("{0:.0f}%".format(word_end_probability*100)) + \
       " of the time"
@@ -145,5 +144,5 @@ model1.buildModel(["apple", "ape", "ample","banana"])
 model1.printList(model1.wordList)
 
 # print out the stats
-model1.printStats(model1.char_couplets, model1.char_starts, model1.word_starts)
+model1.printStats()
 
